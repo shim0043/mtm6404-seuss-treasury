220319afe2a7de8278e8a55a75a6ec7bfb712aa0
Shim Kiseoung <shim0043@algonquinlive.com>
2026-08-06 20:45:45 -0400

    Add nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,11 @@ import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <header className="border-bottom">
-      <div className="container d-flex align-items-center gap-4">
-        <Link to="/" className="text-dark text-decoration-none fw-bold fs-5">
-          Seuss Treasury
-        </Link>
-        <nav className="d-flex gap-3">
+    <header>
+      <div>
+        <Link to="/">Seuss Treasury</Link>
+        
+        <nav>
           <NavLink 
             to="/" 
             className={({ isActive }) => 
