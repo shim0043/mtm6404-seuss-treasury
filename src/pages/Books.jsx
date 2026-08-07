f45a05ef63ceed75d56a5bd6e245626d6ac2e12a
Shim Kiseoung <shim0043@algonquinlive.com>
2026-08-06 20:49:59 -0400

    Add states for books, loading, and errors

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,4 +1,10 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+function Books() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+}
+
 export default Books;
