76ead222e59888bc5211a46abcd930e8441a0869
Shim Kiseoung <shim0043@algonquinlive.com>
2026-08-06 20:32:48 -0400

    Add state for quotes and loading

---
+++ src/pages/Quotes.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+
+function Quotes() {
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+}
+
+export default Quotes;
