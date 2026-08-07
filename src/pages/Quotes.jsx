9602292ceebb9550657dd6fa4e3b54a2e865c84f
Shim Kiseoung <shim0043@algonquinlive.com>
2026-08-06 20:34:38 -0400

    Add quote fetch

--- src/pages/Quotes.jsx
+++ src/pages/Quotes.jsx
@@ -3,6 +3,10 @@ import { useState, useEffect } from 'react';
 function Quotes() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://lustrous-profiterole-958625.netlify.app/api/quotes/random')
+  }, []);
 }
 
 export default Quotes;
